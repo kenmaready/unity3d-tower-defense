4a68222fc7f1f9653087ef513fa162b3d0000604
kenmaready <ken@maready.com>
2023-05-11 09:01:07 -0400

    Add weapon system (bolt from particlesystem) to tower

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cbd6ed8079d0aa858e7f1adc7d61abae364000566a472d8fe56caa6f8d1b905
-size 496
+oid sha256:f998bf7e42fa0cfd724c2e2af8f5cfe5f5be75e9b645645ef32cbce1ccd2295c
+size 592
