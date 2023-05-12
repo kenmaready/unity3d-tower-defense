6c27445913fb25fd758f586bbcc8449fdb17eaa7
kenmaready <ken@maready.com>
2023-05-12 07:55:54 -0400

    Create a true objet pool for the enemy objects

--- Assets/Enemies/EnemyMover.cs
+++ Assets/Enemies/EnemyMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11c5f47223159725de3e5c5b005378634b3520c10705650c320a12eff050fdc6
-size 807
+oid sha256:18e1622d727f8ed6639cea8a9d31d8a718f6e92550b2b90ddd4c72e6ea8323e3
+size 1216
---
+++ Assets/Enemies/ObjectPool.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f885d34d051bf93a84ed69e3563d46a77e4e31f3d83635545b6a2f8ed90984b0
+size 1020
