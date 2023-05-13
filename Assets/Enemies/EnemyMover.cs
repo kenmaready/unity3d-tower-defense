f22c26a9bb5389baf4850a718c19d1cc2666cd2c
kenmaready <ken@maready.com>
2023-05-13 10:29:18 -0400

    Fix path (order) of tiles for enemy movement

--- Assets/Enemies/EnemyMover.cs
+++ Assets/Enemies/EnemyMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abdedf29e84e96df0cfa87525fb39216f95350160d31a2c0fe3cd9d357971fec
-size 1333
+oid sha256:94b9caa23b3d767f129010034928d3322020a945739afc6de84e7960a5ebdc3d
+size 1633
