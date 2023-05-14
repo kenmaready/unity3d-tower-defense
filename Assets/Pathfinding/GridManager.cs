3e6b4fb90d4d916edd3dc9b8b13a0a58b79e01ca
kenmaready <ken@maready.com>
2023-05-14 07:53:26 -0400

    Last commit with fixed path, added TMPro to water tiles to fix error in console

---
+++ Assets/Pathfinding/GridManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c0403d586308b2006b2f96c99dff8ddb4b39faafaa224a716804591d8eb8da3
+size 314
---
+++ Assets/Pathfinding/Node.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a211962efc4ccb62f0d156b4a0902b71dce3cc1876e37b316471e8e81aafc9f
+size 409
